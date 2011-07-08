c3844c481d494f349cd73027c3fd8bcdc667bf7c
Smitt64 <smitt64@yandex.ru>
2011-07-08 20:17:35 +0400

    Rus translation

---
+++ depot_ru_RU.ts
@@ -0,0 +1,206 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>CMainWindow</name>
+    <message>
+        <location filename="src/s_core/cmainwindow.cpp" line="110"/>
+        <source>File</source>
+        <translation>Файл</translation>
+    </message>
+    <message>
+        <location filename="src/s_core/cmainwindow.cpp" line="111"/>
+        <source>New</source>
+        <translation>Создать</translation>
+    </message>
+    <message>
+        <location filename="src/s_core/cmainwindow.cpp" line="112"/>
+        <source>Create new file...</source>
+        <translation>Создать новый файл...</translation>
+    </message>
+    <message>
+        <location filename="src/s_core/cmainwindow.cpp" line="115"/>
+        <source>Open...</source>
+        <translation>Открыть...</translation>
+    </message>
+    <message>
+        <location filename="src/s_core/cmainwindow.cpp" line="116"/>
+        <source>Open exist file...</source>
+        <translation>Открыть существующий файл...</translation>
+    </message>
+    <message>
+        <location filename="src/s_core/cmainwindow.cpp" line="119"/>
+        <source>Save...</source>
+        <translation>Сохранить...</translation>
+    </message>
+    <message>
+        <location filename="src/s_core/cmainwindow.cpp" line="120"/>
+        <source>Save changes...</source>
+        <translation>Сохранить изменения...</translation>
+    </message>
+    <message>
+        <location filename="src/s_core/cmainwindow.cpp" line="123"/>
+        <source>Save as...</source>
+        <translation>Сохранить как...</translation>
+    </message>
+    <message>
+        <location filename="src/s_core/cmainwindow.cpp" line="124"/>
+        <source>Save as new file...</source>
+        <translation>Сохранить как новый файл...</translation>
+    </message>
+    <message>
+        <location filename="src/s_core/cmainwindow.cpp" line="127"/>
+        <source>Close</source>
+        <translation>Закрыть</translation>
+    </message>
+    <message>
+        <location filename="src/s_core/cmainwindow.cpp" line="128"/>
+        <source>Close current file...</source>
+        <translation>Закрыть текущий файл...</translation>
+    </message>
+    <message>
+        <location filename="src/s_core/cmainwindow.cpp" line="132"/>
+        <source>Standart</source>
+        <translation>Стандартная</translation>
+    </message>
+    <message>
+        <location filename="src/s_core/cmainwindow.cpp" line="147"/>
+        <source>Edit</source>
+        <translation>Правка</translation>
+    </message>
+    <message>
+        <location filename="src/s_core/cmainwindow.cpp" line="148"/>
+        <source>Undo</source>
+        <translation>Отменить</translation>
+    </message>
+    <message>
+        <location filename="src/s_core/cmainwindow.cpp" line="150"/>
+        <source>Cancellation of last action...</source>
+        <translation>Отмена последнего действия...</translation>
+    </message>
+    <message>
+        <location filename="src/s_core/cmainwindow.cpp" line="153"/>
+        <source>Redo</source>
+        <translation>Вернуть</translation>
+    </message>
+    <message>
+        <location filename="src/s_core/cmainwindow.cpp" line="155"/>
+        <source>Repeated performance of last action...</source>
+        <translation>Повторное выполнение последнего действия...</translation>
+    </message>
+    <message>
+        <location filename="src/s_core/cmainwindow.cpp" line="165"/>
+        <source>View</source>
+        <translation>Вид</translation>
+    </message>
+    <message>
+        <location filename="src/s_core/cmainwindow.cpp" line="179"/>
+        <source>Status bar</source>
+        <translation>Строка состояния</translation>
+    </message>
+    <message>
+        <location filename="src/s_core/cmainwindow.cpp" line="181"/>
+        <source>Show/hide status bar...</source>
+        <translation>Показать/отобразить строку состояния...</translation>
+    </message>
+    <message>
+        <location filename="src/s_core/cmainwindow.cpp" line="190"/>
+        <source>Full screen</source>
+        <translation>Во весь экран</translation>
+    </message>
+    <message>
+        <location filename="src/s_core/cmainwindow.cpp" line="192"/>
+        <source>Show window full screen...</source>
+        <translation>Показывать окно во весь экран...</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="src/testbuilder/mainwindow.cpp" line="14"/>
+        <source>TestBuiler</source>
+        <translation>Редактор тестов</translation>
+    </message>
+    <message>
+        <location filename="src/testbuilder/mainwindow.cpp" line="20"/>
+        <source>Theme</source>
+        <translation>Тема</translation>
+    </message>
+    <message>
+        <location filename="src/testbuilder/mainwindow.cpp" line="21"/>
+        <source>Add theme</source>
+        <translation>Добавить тему</translation>
+    </message>
+    <message>
+        <location filename="src/testbuilder/mainwindow.cpp" line="22"/>
+        <source>Adding new theme to test...</source>
+        <translation>Добавлять тему в тест...</translation>
+    </message>
+    <message>
+        <location filename="src/testbuilder/mainwindow.cpp" line="26"/>
+        <source>&lt;empty&gt;</source>
+        <translation>&lt;без изменений&gt;</translation>
+    </message>
+    <message>
+        <location filename="src/testbuilder/mainwindow.cpp" line="28"/>
+        <source>Changes list</source>
+        <translation>Список изменений</translation>
+    </message>
+    <message>
+        <location filename="src/testbuilder/mainwindow.cpp" line="33"/>
+        <source>Test struct</source>
+        <translation>Структура теста</translation>
+    </message>
+    <message>
+        <location filename="src/testbuilder/mainwindow.cpp" line="36"/>
+        <source>Elements</source>
+        <translation>Элементы</translation>
+    </message>
+    <message>
+        <location filename="src/testbuilder/mainwindow.cpp" line="52"/>
+        <source>Creating new test...</source>
+        <translation>Создание нового тетса...</translation>
+    </message>
+    <message>
+        <location filename="src/testbuilder/mainwindow.cpp" line="53"/>
+        <source>Test file (*.tst)</source>
+        <translation>Файл тестов (*.tst)</translation>
+    </message>
+    <message>
+        <location filename="src/testbuilder/mainwindow.cpp" line="59"/>
+        <source>Creating error!</source>
+        <translation>Ошибка создания!</translation>
+    </message>
+    <message>
+        <location filename="src/testbuilder/mainwindow.cpp" line="60"/>
+        <source>Can&apos;t create new test!</source>
+        <translation>Не удалось создать новый тест!</translation>
+    </message>
+    <message>
+        <location filename="src/testbuilder/mainwindow.cpp" line="78"/>
+        <source>Test</source>
+        <translation>Тест</translation>
+    </message>
+    <message>
+        <location filename="src/testbuilder/mainwindow.cpp" line="94"/>
+        <source>Addeting new theme...</source>
+        <translation>Добавление новой темы...</translation>
+    </message>
+    <message>
+        <location filename="src/testbuilder/mainwindow.cpp" line="95"/>
+        <source>Enter caption for new theme:</source>
+        <translation>Введите заголовок темы:</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="src/s_core/commands.cpp" line="21"/>
+        <source>New theme: %1.
+Alias: %2</source>
+        <oldsource>New theme: %1. Alias: %2</oldsource>
+        <translation>Добавлена тема: %1. 
+Псевдоним: %2</translation>
+    </message>
+</context>
+</TS>
