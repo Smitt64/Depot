53e45e4ed55fbe8e0bc67dbb8c96aed2a759837a
Smitt64 <smitt64@yandex.ru>
2011-07-18 19:20:46 +0400

    Combobox with checkable items

---
+++ src/s_core/scombobox.cpp
@@ -0,0 +1,48 @@
+#include "scombobox.h"
+#include <QtGui>
+#include <QDebug>
+
+SComboBox::SComboBox(QAbstractItemModel *item_model, QWidget *parent) :
+    QComboBox(parent),
+    skipNextHide(false),
+    str("ghf")
+{
+    setModel(item_model);
+    setView(new QListView(this));
+    view()->viewport()->installEventFilter(this);
+}
+
+void SComboBox::hidePopup() {
+    if (skipNextHide)
+        skipNextHide = false;
+    else
+        QComboBox::hidePopup();
+}
+
+bool SComboBox::eventFilter(QObject* object, QEvent* event) {
+    if (event->type() == QEvent::MouseButtonPress && object == view()->viewport()) {
+        QMouseEvent* mouseEvent = static_cast<QMouseEvent*>(event);
+        QModelIndex index = view()->indexAt(mouseEvent->pos());
+        int checkState = model()->data(index, Qt::CheckStateRole).toInt();
+        model()->setData(index, (checkState == Qt::Checked ? Qt::Unchecked : Qt::Checked), Qt::CheckStateRole);
+        str = "";
+        for(int i = 0; i < model()->rowCount(); i++) {
+            if(model()->data(model()->index(i, 0), Qt::CheckStateRole).toInt() == Qt::Checked)
+                str += model()->data(model()->index(i, 0)).toString() + "; ";
+        }
+            skipNextHide = true;
+    }
+    return false;
+}
+
+void SComboBox::paintEvent(QPaintEvent *e) {
+    QPainter painter(this);
+    QStyleOptionComboBox option;
+    option.initFrom(this);
+    option.editable = false;
+    option.currentText = str;
+    option.frame = true;
+
+    style()->drawComplexControl(QStyle::CC_ComboBox, &option, &painter, this);
+    style()->drawControl(QStyle::CE_ComboBoxLabel, &option, &painter, this);
+}
---
+++ src/s_core/scombobox.h
@@ -0,0 +1,23 @@
+#ifndef SCOMBOBOX_H
+#define SCOMBOBOX_H
+
+#include <QComboBox>
+
+class SComboBox : public QComboBox
+{
+    Q_OBJECT
+public:
+    explicit SComboBox(QAbstractItemModel *item_model, QWidget *parent = 0);
+
+    bool eventFilter(QObject* object, QEvent* event);
+    void hidePopup();
+
+private:
+    QString str;
+    bool skipNextHide;
+
+protected:
+    virtual void paintEvent(QPaintEvent *e);
+};
+
+#endif // SCOMBOBOX_H
